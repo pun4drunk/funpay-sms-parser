758f636c0e1c4f573cf00c22a539909cfc0ea892
Vladislav Waxme <vlad.waxme@gmail.com>
2016-10-09 17:23:48 +0300

    Added: base functionality with test coverage

--- src/SmsParser.php
+++ src/SmsParser.php
@@ -12,10 +12,58 @@ namespace FunPay\SmsParser;
  * @author vladislavs
  */
 
-abstract class SmsParser {
+class SmsParser {
     
-    public static function parse() {
-        
+    /**
+     * Fields used
+     * @var array 
+     */
+    protected $fields = [
+        'code' => NULL, 
+        'amount' => NULL,
+        'currency' => NULL,
+        'recipient' => NULL,
+    ];
+    
+    protected $config = [
+        'patterns' => [
+            "/^Пароль\W+(\w+)$/" => [false, 'code'],
+            "/^Спишется\W+(\d+(?:,\d+))(р)\.?$/" => [false, 'amount', 'currency'],
+            '/^Перевод на счет\W+(\w+)$/' => [false, 'recipient'],
+        ],
+    ];
+    
+    public function __construct(array $config = []) {
+        //populate configuration property with provided overwrites
+        $this->config = array_replace_recursive($this->config, array_intersect_key($config, $this->config));
     }
+    
+    /**
+     * Parse input string using configuration property
+     * @param type $input
+     * @return type
+     */
+    public function parse($string) {
+        
+        $result = $this->fields;
+        $lines = array_map('trim', explode(PHP_EOL, $string));
+        
+        foreach ($this->config['patterns'] as $pattern => $fields) {
+            
+            $count = count($lines);
+            for ($i = 0; $i < $count; $i++) {
+                if (preg_match($pattern, $lines[$i], $matches)) {
+                    $fields = array_filter($fields);
+                    $result = array_merge($result, array_combine($fields, array_intersect_key($matches, $fields)));
+                    array_splice($lines, $i, 1);
+                    break;
+                }
+            }
+        }
+        
+        return array_intersect_key($result, $this->fields);
+    }
+    
+    
     
 }
