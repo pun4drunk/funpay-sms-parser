0225431bafe1127c46580c73b8b181393cf42c7d
Vladislav Waxme <vlad.waxme@gmail.com>
2016-10-09 17:28:54 +0300

    Added: more code comments

--- src/SmsParser.php
+++ src/SmsParser.php
@@ -9,6 +9,13 @@ namespace FunPay\SmsParser;
  * Спишется 100,51р.
  * Перевод на счет 41001174928714
  * 
+ * output example:
+ * [
+ *  'code' => "4102",
+ *  'amount' => "100,51",
+ *  'currency' => 'р',
+ *  'recipient' => '41001174928714',
+ * ]
  * @author vladislavs
  */
 
@@ -52,15 +59,19 @@ class SmsParser {
             
             $count = count($lines);
             for ($i = 0; $i < $count; $i++) {
+                // go through input lines until we have a pattern match
                 if (preg_match($pattern, $lines[$i], $matches)) {
                     $fields = array_filter($fields);
+                    // populate result fields with matches
                     $result = array_merge($result, array_combine($fields, array_intersect_key($matches, $fields)));
+                    // unset processed line
                     array_splice($lines, $i, 1);
                     break;
                 }
             }
         }
         
+        // filter result and return
         return array_intersect_key($result, $this->fields);
     }
     
